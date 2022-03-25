31b373785cfa6901aae36eb627eb591bdcf6c42b
Tyler Witte <tmw2341@gmail.com>
2022-03-25 18:51:26 -0500

    Adding initial cdk project.

---
+++ experiments/test/experiments.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Experiments from '../lib/experiments-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/experiments-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Experiments.ExperimentsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
